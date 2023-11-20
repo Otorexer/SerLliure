29b8863aa2e9a44ae21d5b9b58c1976c06edb27b
Otorexer <118485801+Otorexer@users.noreply.github.com>
2023-11-20 18:18:16 +0100

    Delete Scripts/NextCloud.sh

--- Scripts/NextCloud.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Gets you public ip for later
-publicip=$(curl ipinfo.io/ip)
-
-#Nextcloud-AIO Installer
-sudo docker run --init --sig-proxy=false --name nextcloud-aio-mastercontainer --restart always --publish 80:80 --publish 8080:8080 --publish 8443:8443 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config --volume /var/run/docker.sock:/var/run/docker.sock:ro nextcloud/all-in-one:latest
-
-#Shows portainer addres in console for fast connect
-echo "https://$publicip:8080"
