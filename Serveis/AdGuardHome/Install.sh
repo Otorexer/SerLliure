f4a7f22c8d903a8733efb503b4018a36e70597bc
Otorexer <118485801+Otorexer@users.noreply.github.com>
2023-12-04 09:53:51 +0100

    Update Install.sh

--- Serveis/AdGuardHome/Install.sh
+++ Serveis/AdGuardHome/Install.sh
@@ -28,7 +28,7 @@ docker run --name adguardhome \
     -v "$DATA_DIR":/opt/adguardhome/work \
     -v "$CONFIG_DIR":/opt/adguardhome/conf \
     -p 53:53/tcp -p 53:53/udp\
-    -p 80:80/tcp -p 443:443/tcp -p 443:443/udp -p 3000:3000/tcp\
+    -p 3000:3000/tcp\
     -p 853:853/tcp\
     -p 853:853/udp\
     -p 5443:5443/tcp -p 5443:5443/udp\
