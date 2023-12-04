758e15e7b40b2b5476057576b08f4be0dafe00e6
Otorexer <118485801+Otorexer@users.noreply.github.com>
2023-12-04 12:21:34 +0100

    Update Install.sh

--- Serveis/Portainer-CE/Install.sh
+++ Serveis/Portainer-CE/Install.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-#Gets you public ip for later
-publicip=$(curl ipinfo.io/ip)
-
 #Installs portainer
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-
-#Shows portainer addres in console for fast connect
-echo "https://$publicip:9443"
+docker run -d -p 8000:8000 -p 9443:9443 -p 9001:9001 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
