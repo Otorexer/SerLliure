e62cb7eda768b21229bc273d6b0430ddd479a144
Otorexer <118485801+Otorexer@users.noreply.github.com>
2023-12-04 12:22:42 +0100

    Delete Serveis/Portainer-CE/Install.sh

--- Serveis/Portainer-CE/Install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#Installs portainer
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 -p 9001:9001 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
