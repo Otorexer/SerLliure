1d1d2479dc92f434d026570cfb8b124478180233
Otorexer <118485801+Otorexer@users.noreply.github.com>
2023-12-11 17:29:45 +0100

    Update Install.sh

--- Serveis/Portainer-CE/Install.sh
+++ Serveis/Portainer-CE/Install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #Installs portainer
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 -p --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
