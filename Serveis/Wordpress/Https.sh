787c71937567852a50f706c1dc3a115745d16b78
Otorexer <118485801+Otorexer@users.noreply.github.com>
2024-02-14 18:33:10 +0100

    Create Https.sh

---
+++ Serveis/Wordpress/Https.sh
@@ -0,0 +1,21 @@
+# Creates the direcotry to save ssl
+mkdir /etc/wordpress-https
+
+# Removes any existing ssl
+sudo rm /etc/wordpress-https/key.pem
+sudo rm /etc/wordpress-https/cert.pem
+
+# Generates new key.pem
+sudo openssl genpkey -algorithm RSA -out /etc/wordpress-https/key.pem
+
+#Generates new cert.pem based on the previusly generated key.pem
+sudo openssl req -new -x509 -key /etc/wordpress-https/key.pem -out /etc/wordpress-https/cert.pem -days 365
+
+# Gives permisison to caddy for accesing the files
+sudo chown caddy:caddy /etc/caddy/key.pem
+sudo chown caddy:caddy /etc/caddy/cert.pem
+sudo chmod 600 /etc/caddy/key.pem
+sudo chmod 644 /etc/caddy/cert.pem
+
+# Reloads caddy to apply configuration
+sudo systemctl restart caddy
