58c144a70330db6b8f722761313b7f65586c089a
Otorexer <118485801+Otorexer@users.noreply.github.com>
2024-02-17 23:44:36 +0100

    Update Caddy.sh

--- Serveis/Headscale/Caddy.sh
+++ Serveis/Headscale/Caddy.sh
@@ -2,19 +2,19 @@
 
 # Funció per configurar el proxy invers de Caddy
 configure_caddy_proxy() {
-  if command -v caddy &> /dev/null; then
-    # Extreure el nom del domini 
-    domain_name=$(grep 'server_url:' /etc/headscale/config.yaml | sed -e 's/^.*server_url: //' -e 's|https://||' -e 's|/.*$||')
+  if command -v caddy &> /dev/null; then
+    # Extreure el nom del domini 
+    domain_name=$(grep 'server_url:' /etc/headscale/config.yaml | sed -e 's/^.*server_url: //' -e 's|https://||' -e 's|/.*$||')
 
-    echo "Configurant el proxy invers de Caddy per $domain_name..."
-    echo "$domain_name {" >> /etc/caddy/Caddyfile
-    echo "  reverse_proxy localhost:8080" >> /etc/caddy/Caddyfile
-    echo "}" >> /etc/caddy/Caddyfile
-    sudo systemctl reload caddy
-    echo "Configuració de Caddy actualitzada i recarregada."
-  else
-    echo "Caddy no està instal·lat. Instal·la'l per poder utilitzar aquesta configuració."
-  fi
+    echo "Configurant el proxy invers de Caddy per $domain_name..."
+    echo "$domain_name {" >> /etc/caddy/Caddyfile
+    echo "  reverse_proxy localhost:8080" >> /etc/caddy/Caddyfile
+    echo "}" >> /etc/caddy/Caddyfile
+    sudo systemctl reload caddy
+    echo "Configuració de Caddy actualitzada i recarregada."
+  else
+    echo "Caddy no està instal·lat. Instal·la'l per poder utilitzar aquesta configuració."
+  fi
 }
 
 # Execució principal
