a4698b96a8537af572e2e03759e19780a5ac88dd
Otorexer <118485801+Otorexer@users.noreply.github.com>
2024-02-18 00:32:04 +0100

    Update Install.sh

--- Serveis/NginxProxyManager/Install.sh
+++ Serveis/NginxProxyManager/Install.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
+sudo apt update && sudo apt upgrade -y
 
-# Pull and run the Nginx Proxy Manager Docker container
-docker run -d --name nginxproxy \
-    -p 80:80 \
-    -p 81:81 \
-    -p 443:443 \
-    -v /etc/nginx/data:/data \
-    -v /etc/nginx/letsencrypt:/etc/letsencrypt \
-    jc21/nginx-proxy-manager:latest
+# Directori NginxProxyManager
+directorio="/root/nginxProxyManager"
 
-# Check if the container is running
-echo "Checking if Nginx Proxy Manager is running..."
-docker ps | grep nginxproxy
+# Arxiu docker-compose.yml
+archivo_docker_compose="$directorio/docker-compose.yml"
 
-# Script ends
-echo "Nginx Proxy Manager installation script has completed."
+# Comprovar la instal·lació del Docker
+if ! command -v docker &> /dev/null
+then
+  if wget -qO- https://get.docker.com | sh; then 
+    echo "Docker s'ha instal·lat correctament"
+  else
+    echo "La instal·lació del Docker ha fallat. Si us plau, comproveu els errors."
+    exit 1 # Sortida amb un codi d'error 
+  fi
+else
+  echo "Docker està instal·lat."
+fi
+
+# Verificar si el directori existeix
+if [ ! -d "$directorio" ]; then
+ echo "Creant directori $directorio..."
+ mkdir "$directorio"
+else
+ echo "El directori $directorio_ ja existeix."
+fi
+
+# Verificar si l'arxiu existeix
+if [ ! -f "$archivo_docker_compose" ]; then
+ echo "Descarregant docker-compose.yml"
+ wget https://raw.githubusercontent.com/Otorexer/SerLliure/main/Serveis/Wordpress/docker-compose.yml -O "$archivo_docker_compose"
+else
+ echo "L'arxiu $archivo_docker_compose ja existeix."
+ echo "Si vols editar la configuracio fes servir la següent comanda: sudo nano $archivo_docker_compose"
+fi
+
+cd $directorio
+docker compose up -d
+
+# Afegir un missatge d'èxit bàsic 
+echo "La configuració de NginxProxyManager s'ha iniciat. Consulta la documentació per als següents passos." 
