01333789400d9c95a57465e582df02a2a3b2228d
Otorexer <118485801+Otorexer@users.noreply.github.com>
2024-02-17 22:07:35 +0100

    Update and rename Headscale-Caddy.sh to Caddy.sh

---
+++ Serveis/Headscale/Caddy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Funció per configurar el proxy invers de Caddy
+configure_caddy_proxy() {
+  if command -v caddy &> /dev/null; then
+    # Extreure el nom del domini 
+    domain_name=$(grep 'server_url:' /etc/headscale/config.yaml | sed -e 's/^.*server_url: //' -e 's|https://||' -e 's|/.*$||')
+
+    echo "Configurant el proxy invers de Caddy per $domain_name..."
+    echo "$domain_name {" >> /etc/caddy/Caddyfile
+    echo "  reverse_proxy localhost:8080" >> /etc/caddy/Caddyfile
+    echo "}" >> /etc/caddy/Caddyfile
+    sudo systemctl reload caddy
+    echo "Configuració de Caddy actualitzada i recarregada."
+  else
+    echo "Caddy no està instal·lat. Instal·la'l per poder utilitzar aquesta configuració."
+  fi
+}
+
+# Execució principal
+configure_caddy_proxy
