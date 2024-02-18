49cb958f8a49e0f8c48d0adfc9f1f9117aa45b47
Otorexer <118485801+Otorexer@users.noreply.github.com>
2024-02-18 12:26:28 +0100

    Update Install.sh

--- Serveis/Caddy/Install.sh
+++ Serveis/Caddy/Install.sh
@@ -6,14 +6,14 @@ sudo apt update && sudo apt upgrade -y
 # Prepara el sistema per la instal·lació de Caddy
 
 # Instal·la claus i paquets de transport per a una gestió segura de paquets
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
 
 # Afegeix la clau GPG oficial de Caddy per a la verificació de l'autenticitat del paquet
 # Força la sobreescriptura de qualsevol clau existent
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg --yes 
+sudo curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg --yes 
 
 # Afegeix el repositori Caddy a les fonts de programari del sistema
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 
 # Actualitza la informació del paquet
 sudo apt update
@@ -22,5 +22,10 @@ sudo apt update
 sudo apt install caddy -y
 
 # Finalitza amb un missatge informatiu
-echo "Instal·lació de Caddy completada!"
-echo "Recordeu configurar Caddy segons les vostres necessitats (https://caddyserver.com/docs)"
+if ! command -v caddy &> /dev/null
+then
+  echo "La instal·lació de Caddy ha fallat!"
+else
+  echo "Instal·lació de Caddy completada!"
+  echo "Recordeu configurar Caddy segons les vostres necessitats (https://caddyserver.com/docs)"
+fi
