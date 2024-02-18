0912d6bd642cc6a2e49a1c726992c033fe75d9af
Otorexer <118485801+Otorexer@users.noreply.github.com>
2024-02-18 16:52:25 +0100

    Update Install.sh

--- Serveis/Wordpress/Install.sh
+++ Serveis/Wordpress/Install.sh
@@ -2,10 +2,10 @@
 sudo apt update && sudo apt upgrade -y
 
 # Directori WordPress
-directorio="/root/wordpress"
+directory="/root/wordpress"
 
 # Arxiu docker-compose.yml
-archivo_docker_compose="$directorio/docker-compose.yml"
+docker_compose_file="$directory/docker-compose.yml"
 
 # Comprovar la instal·lació del Docker
 if ! command -v docker &> /dev/null; then
@@ -16,32 +16,32 @@ else
 fi
 
 # Verificar si el directori existeix
-if [ ! -d "$directorio" ]; then
- echo "Creant directori $directorio..."
- mkdir "$directorio"
+if [ ! -d "$directory" ]; then
+ echo "Creant directori $directory..."
+ mkdir "$directory"
 else
- echo "El directori $directorio_ ja existeix."
+ echo "El directori $directory ja existeix."
 fi
 
 # Verificar si l'arxiu existeix
-if [ ! -f "$archivo_docker_compose" ]; then
+if [ ! -f "$docker_compose_file" ]; then
  echo "Descarregant docker-compose.yml"
- wget https://raw.githubusercontent.com/Otorexer/SerLliure/main/Serveis/Wordpress/docker-compose.yml -O "$archivo_docker_compose"
+ wget https://raw.githubusercontent.com/Otorexer/SerLliure/main/Serveis/Wordpress/docker-compose.yml -O "$docker_compose_file"
 
  # Demanar a l'usuari la contrasenya root de MySQL
  read -p "Introdueix la contrasenya root de MySQL: " mysql_root_password
- sed -i "s/MYSQL_ROOT_PASSWORD: wordpress-root/MYSQL_ROOT_PASSWORD: $mysql_root_password/g" /root/wordpress/docker-compose.yml
+ sed -i "s/MYSQL_ROOT_PASSWORD: wordpress-root/MYSQL_ROOT_PASSWORD: $mysql_root_password/g" "$docker_compose_file"
 
  # Demanar a l'usuari el directori per emmagatzemar els volums
  read -p "Introdueix el directori per emmagatzemar els volums (Recomanació: /etc): " volume_directory
- sed -i "s|/etc/wordpress|$volume_directory/wordpress|g" /root/wordpress/docker-compose.yml
- sed -i "s|/etc/wordpress-db|$volume_directory/wordpress-db|g" /root/wordpress/docker-compose.yml
+ sed -i "s|/etc/wordpress|$volume_directory/wordpress|g" "$docker_compose_file"
+ sed -i "s|/etc/wordpress-db|$volume_directory/wordpress-db|g" "$docker_compose_file"
 else
  echo "L'arxiu $archivo_docker_compose ja existeix."
- echo "Si vols editar la configuracio fes servir la següent comanda: sudo nano $archivo_docker_compose"
+ echo "Si vols editar la configuracio fes servir la següent comanda: sudo nano $docker_compose_file"
 fi
 
-cd $directorio
+cd $directory
 docker compose up -d
 
 # Afegir un missatge d'èxit bàsic 
